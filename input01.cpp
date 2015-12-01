b2eec5c12dcfbdfcda22c3d93300ad6fba93006f
serhatcevikel <serhatcevikel@yahoo.com>
2015-12-01 04:09:19 +0200

    Added line

--- input01.cpp
+++ input01.cpp
@@ -14,5 +14,5 @@ int main()
 	cout << "Please enter your first name (followed by 'enter'):\n";
 	string first_name;// first_name is a variable of type string
 	cin >> first_name;// read characters into first_name
-	cout << "Hello, " << first_name << "!\n";
+	cout << "Hello, " << first_name << "!\nHow are you?\n";
 }
