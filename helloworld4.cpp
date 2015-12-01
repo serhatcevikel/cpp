bc476b43d068b086b6dff93f79950f31b3ff127b
serhatcevikel <serhatcevikel@yahoo.com>
2015-12-01 03:03:07 +0200

    Add helloworld4.cpp

---
+++ helloworld4.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
+//#include "std_lib_facilities.h"
+
+int main()
+{
+ cout << "Hello! This is my first C++ program with Ubuntu 14.04\n";
+ /* Do something more if you want */
+ return 0;
+}
