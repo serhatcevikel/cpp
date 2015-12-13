6705d24ed08fd5b891d4b47755e4a2d388b01257
serhatcevikel <serhatcevikel@yahoo.com>
2015-12-13 02:26:43 +0200

    new file

---
+++ 03.04-02.cpp
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
+//#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Please enter your first and second names\n";
+    string first;
+    string second;
+    cin >> first >> second;// read two strings
+    string name = first + ' ' + second;// concatenate strings
+    cout << "Hello, " << name << '\n';
+}
